2f7d9ca8beeb506a846942415b8400a0f1426ca2
Jimmy Kang <blackpearl1210@gmail.com>
2023-09-24 15:04:38 +0900

    initial commit

---
+++ src/main/java/fit/fit/FitApplication.java
@@ -0,0 +1,13 @@
+package fit.fit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitApplication.class, args);
+	}
+
+}
