dd3257377bf2762175e7143ed0aca897b1a9ddcc
Jimmy Kang <blackpearl1210@gmail.com>
2023-09-27 19:41:49 +0900

    미사용 로그 및 소스 제거

--- app/src/main/java/fit/ControllerExceptionHandler.java
+++ app/src/main/java/fit/ControllerExceptionHandler.java
@@ -1,16 +1,13 @@
 package fit;
 
-import fit.command.Signup;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
     @ExceptionHandler(DuplicateEmailException.class)
     public ResponseEntity<Void> handleDuplicateEmailException() {
-        //  추후 로그 입력 시, 변수 입력 -> DuplicateEmailException e
         return ResponseEntity.badRequest().build();
     }
 }
